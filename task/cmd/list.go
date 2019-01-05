1a6e894b2f72cfda3e30e408fc4eee72ce5174d6
vital <373357042@qq.com>
2019-01-05 23:47:47 +0800

    complete task list :kissing_smiling_eyes:

--- task/cmd/list.go
+++ task/cmd/list.go
@@ -1,14 +1,29 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"gophercise/task/db"
+	"os"
 )
 
 var listCmd = &cobra.Command{
-	Use: "list",
+	Use:   "list",
 	Short: "lists your todo list.",
 	Run: func(cmd *cobra.Command, args []string) {
-
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("啊哦...好像出了一点问题:", err.Error())
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Println("你还没有创建过Todo项目哦，赶快创建一个吧")
+			return
+		}
+		fmt.Println("以下是你创建的Todo List:")
+		for i, task := range tasks {
+			fmt.Printf("%d. %s\n", i+1, task.Value)
+		}
 	},
 }
 
