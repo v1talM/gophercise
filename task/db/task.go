aa678cf6c7d0141c5282e1d6a6175eefe7bb51d7
vital <373357042@qq.com>
2019-01-05 23:15:48 +0800

    create task

---
+++ task/db/task.go
@@ -0,0 +1,51 @@
+package db
+
+import (
+	"encoding/binary"
+	"github.com/boltdb/bolt"
+	"time"
+)
+
+var taskBucket = []byte("tasks")
+var db *bolt.DB
+
+type Task struct {
+	Key int
+	Value string
+}
+func Init(dbPath string) error {
+	var err error
+	db, err = bolt.Open(dbPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return err
+	}
+	return db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists(taskBucket)
+		return err
+	})
+}
+
+func Create(task string) (int, error) {
+	var id int
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		id64, _ := b.NextSequence()
+		id = int(id64)
+		key := itob(int(id))
+		return b.Put(key, []byte(task))
+	})
+	if err != nil {
+		return -1, err
+	}
+	return id, nil
+}
+
+func itob(v int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
+}
+
+func btoi(b []byte) int {
+	return int(binary.BigEndian.Uint64(b))
+}
