4cf04fa93aec35a2a476da7f2a216e678fcf1d2d
vital <373357042@qq.com>
2019-01-05 23:51:52 +0800

    complete add task :eyes:

--- task/cmd/add.go
+++ task/cmd/add.go
@@ -1,14 +1,23 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"gophercise/task/db"
+	"strings"
 )
 
 var addCmd = &cobra.Command{
-	Use: "add",
+	Use:   "add",
 	Short: "add a task to your task list.",
 	Run: func(cmd *cobra.Command, args []string) {
-
+		task := strings.Join(args, " ")
+		_, err := db.CreateTask(task)
+		if err != nil {
+			fmt.Println("啊哦...好像出了一点问题:", err.Error())
+			return
+		}
+		fmt.Printf("已添加\"%s\"到Todo list.\n", task)
 	},
 }
 
